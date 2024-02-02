2657a67ec731daf3bc10a0fd073399aba8d03202
Kim Robinson <kimmykokonut@gmail.com>
2024-02-02 11:08:33 -0800

    add edit route and view for client

--- HairSalon/Controllers/ClientsController.cs
+++ HairSalon/Controllers/ClientsController.cs
@@ -33,6 +33,18 @@ public class ClientsController : Controller
     Client thisClient = _db.Clients.FirstOrDefault(client => client.ClientId == id);
     return View(thisClient);
   }
+  public ActionResult Edit(int id)
+  {
+    Client thisClient = _db.Clients.FirstOrDefault(client => client.ClientId == id);
+    return View(thisClient);
+  }
+  [HttpPost]
+  public ActionResult Edit(Client client)
+  {
+    _db.Clients.Update(client);
+    _db.SaveChanges();
+    return RedirectToAction("Index");
+  }
 
 
 
